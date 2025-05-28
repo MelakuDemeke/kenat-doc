a4df3bdca4ec2aace0fb080b222561aad0be1cbe
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-28 18:51:27 +0300

    Refactor code structure for improved readability and maintainability

--- app/layout.js
+++ app/layout.js
@@ -3,20 +3,23 @@ import { Banner, Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
 import './globals.css';
- 
+
 export const metadata = {
   // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
- 
+
 const banner = <Banner storageKey="kenat-info">Kenat is a work in progress project 👨🏻‍💻 </Banner>
 const navbar = (
   <Navbar
-    logo={<b>Kenat</b>}
+    logo={<div className="flex items-center gap-2">
+      <img src="/logo.png" alt="Kenat Logo" className="h-6 w-auto" />
+      <span className="font-bold text-lg">Kenat</span>
+    </div>}
   />
 )
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
- 
+const footer = <Footer>MIT {new Date().getFullYear()} © Kenat by &nbsp; <a target='_blank' href="https://github.com/MelakuDemeke/"> @MelakuDemeke</a>.</Footer>
+
 export default async function RootLayout({ children }) {
   return (
     <html
@@ -37,9 +40,9 @@ export default async function RootLayout({ children }) {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/MelakuDemeke/kenat"
+          docsRepositoryBase="https://github.com/MelakuDemeke/kenat-doc"
           footer={footer}
-          // ... Your additional layout options
+        // ... Your additional layout options
         >
           {children}
         </Layout>
