0d71f39e57fb7ab300b37555edae9b3a74cf9b8c
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-28 15:26:50 +0300

    Add initial layout and styling for Kenat documentation site

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black text-black dark:text-white">
+      <h1 className="text-4xl font-bold">Welcome to Kenat Library</h1>
+    </div>
+  );
+}
