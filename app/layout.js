416f91e8492eb6c97b4fbd9c60ed49dd08d8e738
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-28 18:18:14 +0300

    add banner

--- app/layout.js
+++ app/layout.js
@@ -9,11 +9,10 @@ export const metadata = {
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
  
-// const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
+const banner = <Banner storageKey="kenat-info">Kenat is a work in progress project 👨🏻‍💻 </Banner>
 const navbar = (
   <Navbar
     logo={<b>Kenat</b>}
-
   />
 )
 const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
@@ -35,7 +34,7 @@ export default async function RootLayout({ children }) {
       </Head>
       <body>
         <Layout
-          // banner={banner}
+          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/MelakuDemeke/kenat"
