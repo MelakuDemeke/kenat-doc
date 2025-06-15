ab6336a7a5a24ee2a947538788113445ce6aa3f1
MelakuDemeke <melaku.demeke789@gmail.com>
2025-06-15 19:18:20 +0300

    add record for DateConverter, Clock, and TimeConverter in _meta.js

--- app/doc/Kenat-UI/_meta.js
+++ app/doc/Kenat-UI/_meta.js
@@ -2,4 +2,8 @@ export default {
     Installation: "📦 Installation",
     MonthGrid: "📅 Month Grid",
     DatePicker: "📅 Date Picker",
+    DateConverter: "📅 Date Converter",
+    Clock: "🕒 Clock",
+    TimeConverter: "🕒 Converter"
 }
+  
